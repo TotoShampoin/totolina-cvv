6a2be0de588e62ad73f0a860853b128a5cc72702
TotoShampoin <thomasgarnier2010@hotmail.fr>
2022-01-05 17:22:13 +0100

    Changement des virus

--- src/CVV/CVV_TYPE.c
+++ src/CVV/CVV_TYPE.c
@@ -6,53 +6,53 @@ void initTables() {  //a expliquer dans le rapport
         table_of_chips[0].chips.type  = ALU;
         table_of_chips[0].chips.life  =   2;
         table_of_chips[0].chips.power =   1;
-        table_of_chips[0].chips.price =  10;
+        table_of_chips[0].chips.price =  30; // * 7 = 210
     table_of_chips[1].nom = "RAM";
         table_of_chips[1].chips.type  = RAM;
         table_of_chips[1].chips.life  =   4;
         table_of_chips[1].chips.power =   2;
-        table_of_chips[1].chips.price =  24;
+        table_of_chips[1].chips.price =  60; // * 7 = 420
     table_of_chips[2].nom = "PMMU";
         table_of_chips[2].chips.type  = PMMU;
         table_of_chips[2].chips.life  =    8;
         table_of_chips[2].chips.power =    4;
-        table_of_chips[2].chips.price =   56;
+        table_of_chips[2].chips.price =  120; // * 7 = 840
     table_of_chips[3].nom = "Patchs";
         table_of_chips[3].chips.type  = Patchs;
-        table_of_chips[3].chips.life  =     24;
-        table_of_chips[3].chips.power =      0;
-        table_of_chips[3].chips.price =    176;
+        table_of_chips[3].chips.life  =     16;
+        table_of_chips[3].chips.power =      3;
+        table_of_chips[3].chips.price =    240; // * 7 = 1680
     table_of_chips[4].nom = "Firewall";
         table_of_chips[4].chips.type  = Firewall;
-        table_of_chips[4].chips.life  =       64;
+        table_of_chips[4].chips.life  =       32;
         table_of_chips[4].chips.power =        0;
-        table_of_chips[4].chips.price =      320;
+        table_of_chips[4].chips.price =      360; // * 7 = 2520
     
     table_of_virus[0].nom = "DivZero";
         table_of_virus[0].virus.type  = DivZero;
         table_of_virus[0].virus.life  =       3;
         table_of_virus[0].virus.power =       1;
-        table_of_virus[0].virus.speed =       1;
+        table_of_virus[0].virus.speed =       2;
     table_of_virus[1].nom = "DDOS";
         table_of_virus[1].virus.type  = DDOS;
-        table_of_virus[1].virus.life  =    9;
+        table_of_virus[1].virus.life  =    6;
         table_of_virus[1].virus.power =    2;
-        table_of_virus[1].virus.speed =    2;
+        table_of_virus[1].virus.speed =    3;
     table_of_virus[2].nom = "SegFault";
         table_of_virus[2].virus.type  = SegFault;
-        table_of_virus[2].virus.life  =       11;
+        table_of_virus[2].virus.life  =        3;
         table_of_virus[2].virus.power =        4;
-        table_of_virus[2].virus.speed =        1;
+        table_of_virus[2].virus.speed =        3;
     table_of_virus[3].nom = "MemCorrupt";
         table_of_virus[3].virus.type  = MemCorrupt;
-        table_of_virus[3].virus.life  =         17;
+        table_of_virus[3].virus.life  =         18;
         table_of_virus[3].virus.power =          6;
-        table_of_virus[3].virus.speed =          3;
+        table_of_virus[3].virus.speed =          1;
     table_of_virus[4].nom = "Bug";
         table_of_virus[4].virus.type  = Bug;
-        table_of_virus[4].virus.life  =  21;
-        table_of_virus[4].virus.power =   8;
-        table_of_virus[4].virus.speed =   4;
+        table_of_virus[4].virus.life  =  15;
+        table_of_virus[4].virus.power =   4;
+        table_of_virus[4].virus.speed =   2;
 }
 
 int getChipsIndexFromType(ChipsType type) {
